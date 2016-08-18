fc21672cbb5d4f35666d286d3d9dcdd9acdcbe12
Chris Scott <christocracy@gmail.com>
2016-08-17 23:43:49 -0400

    add plaform references

--- background-geolocation.ios.d.ts
+++ background-geolocation.ios.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="node_modules/tns-platform-declarations/tns-core-modules/ios.d.ts" />
 import { AbstractBackgroundGeolocation } from './background-geolocation.common';
 export declare class BackgroundGeolocation extends AbstractBackgroundGeolocation {
     constructor();
