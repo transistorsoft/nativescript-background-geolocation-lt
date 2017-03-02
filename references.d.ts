33dc2d9360d140cec38cf158b9a188d8039e8b81
Chris Scott <christocracy@gmail.com>
2017-03-02 13:17:46 -0500

    Provide declarations to ignore missing references

--- references.d.ts
+++ references.d.ts
@@ -1,4 +1,9 @@
-/// <reference path="./node_modules/tns-core-modules/lib.core.d.ts" /> Needed for autocompletion and compilation.
-/// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" /> Needed for autocompletion and compilation.
+/// <reference path="./node_modules/tns-core-modules/lib.core.d.ts" /> 
+/// <reference path="./node_modules/tns-core-modules/tns-core-modules.es6.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/ios/ios.d.ts" />
+
+declare type FormData = any;
+declare var FormData;
+declare type Blob = any;
+declare var Blob;
