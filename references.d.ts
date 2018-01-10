347c9a8932b474ea368191ad99fc55656f160c13
Chris Scott <christocracy@gmail.com>
2018-01-10 11:31:22 -0500

    Fix issues with tsc compilation.  Now compiles clean with no errors.  Add install-local because recent versions of node install local packages as symlinks

--- references.d.ts
+++ references.d.ts
@@ -1,8 +1,3 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
 
-declare type FormData = any;
-declare type Headers = any;
-declare type RequestInit = any;
-declare type Response = any;
-declare type Console = any;
