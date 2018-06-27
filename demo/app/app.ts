d78c13af483ea08c94fbd9fa230fc9b24fab54ea
Chris Scott <christocracy@gmail.com>
2018-06-27 13:28:36 -0400

    Fix Android Headless mode.  missing AndroidManifest.xml.

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -17,11 +17,39 @@ TNSFontIcon.loadCss();
 
 let resources = application.getResources();
 resources['fonticon'] = fonticon;
-//resources['isActive'] = isActive;
 application.setResources(resources);
+
+
+// iOS BackgroundFetch Setup
+if (application.ios) {
+  class MyDelegate extends UIResponder implements UIApplicationDelegate {
+    public static ObjCProtocols = [UIApplicationDelegate];
+
+    public applicationPerformFetchWithCompletionHandler(application: UIApplication, completionHandler:any) {
+      BackgroundFetch.performFetchWithCompletionHandler(completionHandler, application.applicationState);
+    }
+  }
+  application.ios.delegate = MyDelegate;
+}
 
 application.run({ moduleName: 'app-root' });
 
+
+// Android HeadlessTask Setup
+if (application.android) {
+    // BackgroundGeolocation Headless Setup
+    BackgroundGeolocation.registerHeadlessTask((event, completionHandler) => {
+    console.log('[My Headless Task: ', event.name, event.params);
+    // Do stuff.
+    completionHandler();  // <-- signal completion of your HeadlessTask
+  });
+
+  // BackgroundFetch Headless Setup
+  BackgroundFetch.registerHeadlessTask(() => {
+    console.log('[My Headless BackgroundFetch Task]');
+    BackgroundFetch.finish();
+  });
+}
 /*
 Do not place any code after the application has been started as it will not
 be executed on iOS.
